f0d7e18e66112ed7144e62b5097ffed193ba486b
Michał Rokita <mrokita@mrokita.pl>
2020-01-28 18:50:06 +0100

    32bit version + minor fixes

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 all:
-	gcc -g -m64	-std=c99 -o main.o -c main.c
+	gcc -g -m64 -std=c99 -o main.o -c main.c
 	nasm -f elf64 sudoku.s
 	gcc -g -m64 -o sudoku main.o sudoku.o
+32bit:
+	gcc -g -m32 -std=c99 -o main.o -c main.c
+	nasm -f elf32 sudoku.i386.s -o sudoku.o
+	gcc -g -m32 -o sudoku main.o sudoku.o
 clean:
 	@rm -f *.o
 	@rm -f sudoku
--- sudoku.i386.s
+++ sudoku.i386.s
@@ -7,128 +7,136 @@ global sudoku
 %define CHECK_FAILED  0
 
 ; [[ "Zmienne globalne" ]]
-; R13 - sudoku
+; EDI - sudoku
 
-; [[ Funkcja zwracająca do AX wartość komórki (DI, SI) ]]
-; DIL - X
-; SIL - Y
+; [[ Funkcja zwracająca do AX wartość komórki (BL, CL) ]]
+; BL - X
+; CL - Y
 ; AL - Wartość komórki
 get_cell_value:
-    mov al, sil
+    xor eax, eax
+    mov al, cl
     lea eax, [eax*8 + eax]   ; Wyciągnięcie wartości z adresu i wrzucenie do AX
-    add al, dil
-    mov al, [r13+rax]
+    add al, bl
+    mov al, [edi + eax]
     ret
 
 ; [[ Funkcja zapisująca wartość komórki (DI, SI) ]]
-; DIL - X
-; SIL - Y
+; BL - X
+; CL - Y
 ; DL - Wartość komórki
 set_cell_value:
-    mov al, sil
+    xor eax, eax
+    mov al, cl
     lea eax, [eax*8 + eax]
-    add al, dil
-    mov [r13+rax], dl
+    add al, bl
+    mov [edi + eax], dl
     ret
 
 ; [[ Funkcja sprawdzająca, czy wstawienie wartości DX do komórki (DI, SI) jest możliwe, po pudełku 3x3 ]]
-; DIL - X
-; SIL - Y
+; BL - X
+; CL - Y
 ; DL - Wartość sprawdzana
 ; AL - CHECK_PASSED, jeśli można, CHECK_FAILED, jeśli nie
 check_box:
-    xor r8b, r8b
-    xor r9b, r9b
-    mov r8b, dil ; X sprawdzanej komórki
-    mov r9b, sil ; Y sprawdzanej komórki
+    xor bh, bh
+    xor ch, ch
+    mov bh, bl ; X sprawdzanej komórki
+    mov ch, cl ; Y sprawdzanej komórki
 _check_box_calc_offset_x:
-    xor r10b, r10b ; r10w X offset
+    xor dh, dh ; r10w X offset
 _check_box_loop_box_offset_x:
-    add r10b, 3
-    cmp r10b, dil
+    add dh, 3
+    cmp dh, bl
     jle _check_box_loop_box_offset_x
-    sub r10w, 3
+    sub dh, 3
 _check_box_calc_offset_y:
-    xor r11b, r11b
+    xor ah, ah
 _check_box_loop_box_offset_y:
-    add r11b, 3
-    cmp r11b, sil
+    add ah, 3
+    cmp ah, cl
     jle _check_box_loop_box_offset_y
-    sub r11b, 3
+    sub ah, 3
 _check_box_loop_start:
-    inc dil
-    add r10b, 2 ; zwiększenie offsetu o 2 do porownania cur_x >= offset_x + 2
-    cmp dil, r10b
+    inc bl
+    add dh, 2 ; zwiększenie offsetu o 2 do porownania cur_x >= offset_x + 2
+    cmp bl, dh
     jle _check_box_skip_col_inc
 _check_box_col_inc:
-    mov dil, r10b
-    sub dil, 2
-    inc sil
+    mov bl, dh
+    sub bl, 2
+    inc cl
 _check_box_skip_col_inc:
-    sub r10b, 2;
+    sub dh, 2;
 
-    add r11b, 2 ; zwiększenie offsetu o 2 do porownania cur_y >= offset_y + 2
-    cmp sil, r11b
+    add ah, 2 ; zwiększenie offsetu o 2 do porownania cur_y >= offset_y + 2
+    cmp cl, ah
     jle _check_box_skip_row_inc
 _check_box_row_inc:
-    mov sil, r11b
-    sub sil, 2
+    mov cl, ah
+    sub cl, 2
 _check_box_skip_row_inc:
-    sub r11b, 2
+    sub ah, 2
 
-    cmp dil, r8b
+    cmp bl, bh
     jne _check_box_check_eq
-    cmp sil, r9b
+    cmp cl, ch
     jne _check_box_check_eq
     ; Sprawdzona całość, zwracamy CHECK_PASSED
     jmp _check_box_ret_passed
 _check_box_check_eq:
+c1:
+    push eax
     call get_cell_value
     cmp al, dl
+    pop eax
+c2:
     jne _check_box_loop_start
 _check_box_ret_failed:
-    xor rax, rax
-    mov sil, r9b
-    mov dil, r8b
+    xor al, al
+    mov cl, ch
+    mov bl, bh
     mov al, CHECK_FAILED
     ret
 _check_box_ret_passed:
     xor al, al
-    mov sil, r9b
-    mov dil, r8b
+    mov cl, ch
+    mov bl, bh
     mov al, CHECK_PASSED
     ret
 
 ; [[ Funkcja sprawdzająca, czy wstawienie wartości DX do komórki (DI, SI) jest możliwe (po kolumnie)]]
-; DIL - X
+; bl - X
 ; SI - Y
 ; DL - Wartość sprawdzana
 ; AL - CHECK_PASSED, jeśli można, CHECK_FAILED, jeśli nie
 check_col:
     ; R8W - sprawdzany X
-    xor r8b, r8b
-    xor r9b, r9b
-    mov r8b, sil ; Przeniesienie si do r9w, aby zachowac Y sprawdzanego pola
+    xor bh, bh
+    xor ch, ch
+    mov bh, cl ; Przeniesienie si do r9w, aby zachowac Y sprawdzanego pola
 _check_col_loop_start:
-    inc sil
-    cmp sil, 8
+    inc cl
+    cmp cl, 8
     jle _check_col_loop_scroll_end ; Jeśli aktualny X nie przeskoczył za zakres, przejście za operację "przewinięcia"
 _check_col_loop_scroll_start:
-    mov sil, 0
+    xor cl, cl
 _check_col_loop_scroll_end:
-    cmp r8b, sil ; Czy wróciliśmy do badanego Ya
+    cmp bh, cl ; Czy wróciliśmy do badanego Ya
     je _check_col_ret_passed
+    push eax
     call get_cell_value
     cmp al, dl
+    pop eax
     jne _check_col_loop_start
 _check_col_ret_failed:
     xor al, al
-    mov sil, r8b
+    mov cl, bh
     mov al, CHECK_FAILED
     ret
 _check_col_ret_passed:
     xor al, al
-    mov sil, r8b
+    mov cl, bh
     mov al, CHECK_PASSED
     ret
 
@@ -140,60 +148,65 @@ _check_col_ret_passed:
 ; AX - CHECK_PASSED, jeśli można, CHECK_FAILED, jeśli niehttps://www.facebook.com/
 check_row:
     ; R8W - sprawdzany X
-    xor r8b, r8b
-    xor r9b, r9b
-    mov r8b, dil ; Przeniesienie, żeby zachować di do zakonczenia iteracji
+    xor bh, bh
+    xor ch, ch
+    mov bh, bl ; Przeniesienie, żeby zachować di do zakonczenia iteracji
 _check_row_loop_start:
-    inc dil
-    cmp dil, 8
+    inc bl
+    cmp bl, 8
     jle _check_row_loop_scroll_end ; Jeśli aktualny X nie przeskoczył za zakres, przejście za operację "przewinięcia"
 _check_row_loop_scroll_start:
-    mov dil, 0
+    xor bl, bl
 _check_row_loop_scroll_end:
-    cmp r8b, dil ; Czy wróciliśmy do badanego Xa
+    cmp bh, bl ; Czy wróciliśmy do badanego Xa
     je _check_row_ret_passed
+    push eax
     call get_cell_value
     cmp al, dl
+    pop eax
     jne _check_row_loop_start
 _check_row_ret_failed:
     xor al, al
-    mov dil, r8b
+    mov bl, bh
     mov al, CHECK_FAILED
     ret
 _check_row_ret_passed:
     xor al, al
-    mov dil, r8b
+    mov bl, bh
     mov al, CHECK_PASSED
     ret
 
 
 ; [[ Funkcja  ]]
-; DI - X
-; SI - Y
-; AX - c_PASSED, jeśli udało się znaleźć wartość dla aktualnego i każdego następnego, GUESS_FAILED, jeśli nie nie udało
+; BL - X
+; CL - Y
+; AL - c_PASSED, jeśli udało się znaleźć wartość dla aktualnego i każdego następnego, GUESS_FAILED, jeśli nie nie udało
 guess:
-    push rbp;
-    xor rax, rax;
-    mov rbp, rsp;
+    push ebp;
+    xor al, al;
+    mov ebp, esp;
 
 _guess_first_empty_loop_start: ; Pętla szukająca pierwszego pustego miejsca
-    inc dil
-    cmp dil, 9
+    inc bl
+    cmp bl, 9
     jne _guess_skip_inc_row
-_guess_inc_row: ;
-    inc sil
-    xor dil, dil
+_guess_inc_row:
+    inc cl
+    xor bl, bl
 _guess_skip_inc_row:
-    cmp sil, 8
+    cmp cl, 8
     jg _guess_ret_passed
+    push eax
     call get_cell_value
     cmp al, 0
+    pop eax
     jne _guess_first_empty_loop_start
 
-    xor rdx, rdx ; Wartość sprawdzana,
+    xor dl, dl ; Wartość sprawdzana,
 _guess_guessing_loop_start:
     inc dl
     cmp dl, 9
+b1:
     jg _guess_ret_failed
     call check_row
     cmp al, CHECK_PASSED
@@ -206,44 +219,54 @@ _guess_guessing_loop_start:
     call check_box
     cmp al, CHECK_PASSED
     jne _guess_guessing_loop_start
+
+b2:
+    push eax
     call set_cell_value
+    pop eax
 bbp:
-    push rdi
-    push rsi
-    push rdx
+
+    push edi
+    push ebx
+    push ecx
+    push edx
     call guess
-    pop rdx
-    pop rsi
-    pop rdi
+    pop edx
+    pop ecx
+    pop ebx
+    pop edi
 bap:
     cmp al, GUESS_PASSED
     jne _guess_guessing_loop_start
 _guess_ret_passed:
     xor al, al
     mov al, GUESS_PASSED
-    pop rbp
+    pop ebp
     ret
 _guess_ret_failed:
     xor dl, dl
+    push eax
     call set_cell_value
+    pop eax
     xor al, al
     mov al, GUESS_FAILED
-    pop rbp
+    pop ebp
     ret
 
 sudoku:
-    push rbp;
-    mov rbp, rsp;
-    mov r13, rdi ; Przeniesienie adresu sudoku na R13
-    xor rdi, rdi
-    xor rsi, rsi ; Początkowa pozycja na zero
+    push ebp;
+    mov ebp, esp;
+    mov edi, [ebp+8] ; Przeniesienie adresu sudoku na edi
+    xor ecx, ecx
+    xor ebx, ebx ; Początkowa pozycja na zero
+    xor edx, edx
+    xor eax, eax
 
-    xor rdx, rdx
-    DEC dil ; ustawienie na -1 do guess
+     dec bl ; ustawienie na -1 do guess
     ; Wywołanie guess
     ; RDI - kolumna
     ; RSI - wiersz
     call guess
 end:
-    pop rbp
+    pop ebp
     ret
